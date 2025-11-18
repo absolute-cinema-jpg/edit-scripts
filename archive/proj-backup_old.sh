4249ce96b3aac0ceca1ba8f8024e488584559bf8
absolute-cinema-jpg <4lex99@googlemail,com>
2025-11-18 10:59:38 +0000

    IT WORKS

--- archive/proj-backup_old.sh
+++ archive/proj-backup_old.sh
@@ -1,16 +1,11 @@
 #!/bin/zsh
 
-project_location="/Volumes/IS_Nexis/IN_STARLAND"
-backup_location="/Users/ff_tc_25006/Documents/Backup/"
-
-project_folder_name=$(basename $project_location)
-
-osascript -e 'display notification "Backup started" with title "Backup Job" sound name "Funk"'
+project_location=/Volumes/IS_Nexis/IN_STARLAND
+backup_location=/Users/ff_tc_25006/Documents/Backup
+project_folder_name=IN_STARLAND
 
 #Copy the whole folder to backup location (ideally Documents)
-cp -R $project_location $backup_location
-
-osascript -e 'display notification "Zipping backup" with title "Backup Job" sound name "Frog"'
+rsync -avPh $project_location $backup_location
 
 #Archive the freshly copied folder
 zip -r $backup_location/$(date +%y%m%d)_$project_folder_name.zip $backup_location/$project_folder_name
--- archive/testNew.sh
+++ archive/testNew.sh
@@ -2,5 +2,5 @@
 # Redirect all output (stdout & stderr) to a log file
 exec >"$HOME/testlog.txt" 2>&1
 
-say "test new"
+say "test alarm"
 cp -R "$HOME/Downloads/Archive" "$HOME/Desktop"
